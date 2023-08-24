16f7d9b3e98f0d4288cc416b8b859c148fff56e8
YuriyEG <yuriygermanovich0202@gmail.com>
2023-08-24 22:07:04 +0300

    footer => class component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,24 @@
 import React from "react";
 
+
 import TasksFilter from '../TasksFilter/TasksFilter.js';
 
 
-function Footer ({todoCount}) {
 
+class Footer extends React.Component  {
+
+  render() {
+    
     return (
         <footer className="footer">
-        <span className="todo-count">{todoCount} items left</span>
+        <span className="todo-count">{this.props.todoCount} items left</span>
         <TasksFilter/>
         <button className="clear-completed">Clear completed</button>
       </footer>
       
     )
+    
+  }
 }
 
 export default Footer;
