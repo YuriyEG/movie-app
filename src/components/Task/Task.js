3ac3174099914f968e2dafe5ca10fcfd70209e49
YuriyEG <yuriygermanovich0202@gmail.com>
2023-08-27 12:05:33 +0300

    edit form creating start

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,6 +3,7 @@ import { formatDistanceToNow } from 'date-fns';
 // import PropTypes from 'prop-types';
 
 import './Task.css';
+import Edit from '../Edit/Edit';
 
 function Task({ value, deleteTask, id, onToggleImportant, onToggleDone, important, done, time }) {
   let taskClass = 'description';
@@ -16,19 +17,22 @@ function Task({ value, deleteTask, id, onToggleImportant, onToggleDone, importan
   const distance = formatDistanceToNow(time, { includeSeconds: true });
 
   return (
-    <li>
-      <div className="view">
-        <input className="toggle" id="check" onChange={onToggleImportant} type="checkbox" />
-        <label>
-          <span className={taskClass} onClick={onToggleDone}>
-            {value}
-          </span>
-          <span className="created">created {distance} ago</span>
-        </label>
-        <button className="icon icon-edit"></button>
-        <button className="icon icon-destroy" onClick={() => deleteTask(id)}></button>
-      </div>
-    </li>
+    <div>
+      <li>
+        <div className="view">
+          <input className="toggle" id="check" onChange={onToggleImportant} type="checkbox" />
+          <label>
+            <span className={taskClass} onClick={onToggleDone}>
+              {value}
+            </span>
+            <span className="created">created {distance} ago</span>
+          </label>
+          <button className="icon icon-edit"></button>
+          <button className="icon icon-destroy" onClick={() => deleteTask(id)}></button>
+        </div>
+      </li>
+      <Edit />
+    </div>
   );
 }
 
