414f19f023ee7b85e5d07aaf55afd172a029fda8
YuriyEG <yuriygermanovich0202@gmail.com>
2023-08-27 06:13:39 +0300

    after deployment  -- vercel2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import TodoApp from './components/TodoApp.js/TodoApp';
+import TodoApp from './components/TodoApp/TodoApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
