c945475a1c83fada6e46507be564c4643b5d8b10
YuriyEG <you@example.com>
2023-09-10 11:43:26 +0300

    lint:fix for Card successfully

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,23 +1,19 @@
-/* eslint-disable */
 import React from 'react';
-import editDescription from '../../modules/editDescription';
 
+import editDescription from '../../modules/editDescription';
 import transformDate from '../../modules/transformDate';
 import Stars from '../Stars';
 import '../Stars/Stars.css';
 import getColor from '../../modules/getColor';
 
-
-const Card = ({ film,  genresObj, rateCard }) => {
-
+const Card = ({ film, genresObj, rateCard }) => {
   const overview = editDescription(film.overview, 140, '...');
   const title = editDescription(film.title, 20, '');
   const average = film.vote_average.toFixed(1);
   const date = transformDate(film.release_date);
-  const rating = film.rating;
+  const { rating } = film;
   const color = getColor(film.vote_average);
 
-  
   const imageUrl = film.poster_path;
   let posterImage;
   if (imageUrl === null) {
@@ -25,38 +21,43 @@ const Card = ({ film,  genresObj, rateCard }) => {
   } else {
     posterImage = `https://image.tmdb.org/t/p/w500${imageUrl}`;
   }
-  
+
   const RateFilm = (rate) => {
     rateCard(rate, film.id);
-  }
-  
-  let ids = film.genre_ids;
-  let genresTrain = [];
-  for (let i=0; i<5; i++) {
-    if  (ids[i]) {
+  };
+
+  const ids = film.genre_ids;
+  const genresTrain = [];
+  for (let i = 0; i < 5; i += 1) {
+    if (ids[i]) {
       genresTrain.push(genresObj[ids[i]]);
     }
   }
-  
+
   return (
     <div className="card">
       <div className="card__picture">
-        <img className='card__image' src={posterImage} alt=''></img>
+        <img className="card__image" src={posterImage} alt=""></img>
       </div>
       <div className="card__info">
         <div className="card__header">
           <span className="card__title">{title} </span>
-          <div className="card__rating" style={{ border: `3px solid ${color}`}}>{average}</div>
+          <div className="card__rating" style={{ border: `3px solid ${color}` }}>
+            {average}
+          </div>
         </div>
         <span className="card__date">{date}</span>
         <div className="card__genres">
-          { genresTrain.map( genre => <div key={Math.random()} className="card__genres-item">{genre}</div> )}
-      
+          {genresTrain.map((genre) => (
+            <div key={Math.random()} className="card__genres-item">
+              {genre}
+            </div>
+          ))}
         </div>
         <div className="card__text">{overview}</div>
 
         <div className="card__component-wrapper">
-          <Stars rating={rating} rateById={RateFilm}/>
+          <Stars rating={rating} rateById={RateFilm} />
         </div>
       </div>
     </div>
