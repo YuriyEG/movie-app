d37d45dd96b3f3d2932814163f213b4b68112bf9
YuriyEG <you@example.com>
2023-09-10 06:42:36 +0300

    debounced module added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import debounceMaker from './modules/Debounce';
 import MovieApp from './components/MovieApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
